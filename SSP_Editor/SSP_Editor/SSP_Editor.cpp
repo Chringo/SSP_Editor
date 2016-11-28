8a5a1a2bf65470b6510039504bdd96e593689ab0
David Wigelius <david.wigelius@gmail.com>
2016-11-28 13:56:46 +0100

    UPDATE: refined the functions

--- SSP_Editor/SSP_Editor/SSP_Editor.cpp
+++ SSP_Editor/SSP_Editor/SSP_Editor.cpp
@@ -5,14 +5,20 @@ SSP_Editor::SSP_Editor(QWidget *parent)
 {
 	ui.setupUi(this);
 
+	/*making a fileSystemModel to enable a file viewer in the treeView widget*/
 	this->model = new QFileSystemModel(this);
-	this->model->setRootPath("C:/");
+	this->model->setRootPath("C:/Users/Cool_David_92/Desktop/hehee");
 	this->model->setFilter(QDir::NoDotAndDotDot | QDir::Files);
 	
+	/*a list of filters for the treeView*/
 	QStringList filters;
 	filters << "*.bbf";
 
+	/*setting the filters and disabling the user from seeing any other files*/
 	this->model->setNameFilters(filters);
+	this->model->setNameFilterDisables(false);
+
+	/*setting the fileSystemModel to the treeView and connecting the signal slot*/
 	ui.treeView->setModel(this->model);
 	connect(ui.treeView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(treeView_doubleClicked()));
 }
@@ -31,8 +37,8 @@ void SSP_Editor::treeView_doubleClicked()
 	if (!index.isValid()) return;
 	
 	QFileInfo fileInfo = this->model->fileInfo(index);
-	QString test = fileInfo.filePath();
+	QString filePath = fileInfo.filePath();
 
-	printf("hejsan");
+	/*send the filepath to the importer*/
 
 }
